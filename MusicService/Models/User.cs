16ca0b76b567889b56e6770b542ca7559f617071
Mortmeister <morlillehaug@hotmail.com>
2026-04-07 20:10:23 +0200

    Feat: Added shared data store for users, events, bookings and reviews

--- MusicService/Models/User.cs
+++ MusicService/Models/User.cs
@@ -2,20 +2,25 @@ using MusicService.Models.Events;
 
 namespace MusicService.Models;
 
-
-// A user has a username and password, a list of bookings they´ve made, a list of reviews that they made, and reviews they´ve received. 
-// They also have a list of events they´ve published. 
 public class User
 {
-    public string _username { get; private set; }
-    public string _passwordHash { get; private set; }
-    public List<Booking> BookingHistory { get;} = new List<Booking>();
-    public List<Review> ReviewHistory { get;} = new List<Review>(); 
-    public List<Event> MyEvents { get;} = new List<Event>();
+    public string Username { get; private set; }
+    public string PasswordHash { get; private set; }
     
     public User(string username, string password)
     {
-        _username = username;
-        _passwordHash = password;
+        if (string.IsNullOrWhiteSpace(username))
+        {
+            throw new ArgumentException("Username cannot be empty");
+        }
+
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            throw new ArgumentException("Password cannot be empty");
+        }
+        
+        
+        Username = username;
+        PasswordHash = password;
     }
 }
