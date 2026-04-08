1009b070463a6cda2a4bd5ef182de2aae182d73c
Mortmeister <morlillehaug@hotmail.com>
2026-04-08 22:04:34 +0200

    Feat: Added create concert logic

--- MusicService/Program.cs
+++ MusicService/Program.cs
@@ -1,4 +1,7 @@
 ﻿using MusicService.Data;
+using MusicService.Enums;
+using MusicService.Models;
+using MusicService.Services;
 
 namespace MusicService;
 
@@ -7,6 +10,27 @@ class Program
     
     static void Main(string[] args)
     {
-        var DataStorage = new DataStorage();
+        
+        
+        var dataStorage = new DataStorage();
+        var eventService = new EventService(dataStorage);
+        
+        /// Generated dummy variables for testing
+        var testUser = new User("Arvid","Arvid");
+        
+        var tickets = new List<TicketType>
+        {
+            new TicketType("Early Bird", 199, 50),
+            new TicketType("Standard", 350, 100),
+            new TicketType("VIP", 699, 20)
+        };
+
+        var performers = new List<string> { "Aurora", "Sigrid" };
+
+        eventService.CreateConcert("Nordic Nights", "A great show", EventCategory.Pop,
+            DateTime.Now.AddDays(30), "Spektrum, Oslo", testUser, tickets, performers, "Indie Pop");
+        
+        Console.WriteLine(dataStorage.Events.First().GetSummary());
+        ///
     }
 }
