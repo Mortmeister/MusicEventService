db080ac25a9a06e051160fcb1447407bc441c069
vicenteM67 <vicenteakiraqm@gmail.com>
2026-04-19 15:49:36 +0200

    Add files via upload
    Changed Login to return  user as an object instead of boolean

---
+++ MusicService/Services/LoginService.cs
@@ -0,0 +1,31 @@
+﻿using MusicService.Data;
+using MusicService.Models;
+namespace MusicService.Services;
+
+public class LoginService 
+{
+    private readonly DataStorage _repo;
+    public User? CurrentUser { get; private set; } 
+
+    public LoginService(DataStorage repo) 
+    {
+        _repo = repo;
+    }
+
+    public User? Login(string username, string password)
+    {
+        var user = _repo.Users.FirstOrDefault(u => u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+
+        string hashedInput = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(password));
+
+        if (user != null && user.PasswordHash == hashedInput)
+        {
+            CurrentUser = user;
+            return user; 
+        }
+
+        CurrentUser = null;
+        return null; 
+    }
+    public void Logout() => CurrentUser = null;
+}
