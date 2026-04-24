c755468db017a44984a3e89c265fa80da4a3600c
vicenteM67 <vicenteakiraqm@gmail.com>
2026-04-24 11:37:46 +0200

    Add files via upload

---
+++ MusicService/Program.cs
@@ -0,0 +1,37 @@
+﻿using MusicService.Data;
+using MusicService.Enums;
+using MusicService.Models;
+using MusicService.Services;
+using MusicService.Models.Events;
+using MusicService.UI.Menus;
+
+namespace MusicService;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        
+        var dataStorage = new DataStorage();
+        var eventService = new EventService(dataStorage);
+        var registerService = new RegisterService(dataStorage);
+        var loginService = new LoginService(dataStorage);
+        var guestMenu = new GuestMenu(registerService, loginService);
+        /*var mainMenu = new MainMenu(eventService);*/
+
+        var testUser = registerService.Register("test", "test");
+        var reviewService = new ReviewService(dataStorage);
+        
+
+        while (true)
+        {
+            User? loggedIn = guestMenu.ShowGuestMenu();
+            if (loggedIn != null)
+            {
+                var mainMenu = new MainMenu(eventService, reviewService, loggedIn);
+                mainMenu.ShowMainMenu();
+            }
+        }
+    }
+}
